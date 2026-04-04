0d9cd60a742ae8ffc3f20c2342efbe7100c4311e
swaritnarang <naranswarit02@gmail.com>
2026-04-03 22:18:27 -0500

    fix: replace same image URL with varied images

--- src/lib/carousel.ts
+++ src/lib/carousel.ts
@@ -1,13 +1,13 @@
 export const carouselImages = [
   'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg',
-  'https://cdn.hackclub.com/019d546b-fc5d-79b2-98ac-0b4976207567/1000005428.jpg'
+  'https://cdn.hackclub.com/019d567a-2b89-7fcd-966c-f21a5b312ed0/IMG_20250907_191530884_HDR.jpg',
+  'https://cdn.hackclub.com/019d567a-1dda-701e-ac87-2b7dea1abee6/IMG_20250907_173513704_HDR.jpg',
+  'https://cdn.hackclub.com/019d567a-0d64-7b95-900e-0945ea0a2524/IMG_5928.jpeg',
+  'https://cdn.hackclub.com/019d567e-683b-7292-a774-25d655190a0e/img_7616.jpg',
+  'https://cdn.hackclub.com/019d5679-d8b1-7903-92ab-c00496c13ee0/DSC04099.JPG',
+  'https://cdn.hackclub.com/019d5679-ca0a-7f99-b2a9-134e545cb957/03.webp',
+  'https://cdn.hackclub.com/019d567c-6f1b-7e77-a0c3-d4f1f605c2b3/IMG_20250817_202012327.jpg',
+  'https://cdn.hackclub.com/019d567c-6005-744f-a28b-6de567b67bb0/IMG_9045.JPG',
+  'https://cdn.hackclub.com/019d567c-515c-7220-8860-472a1d562c0c/IMG_7610.jpg',
+  'https://cdn.hackclub.com/019d5679-fdab-7961-9a3e-f3a9b7d37023/IMG_2090.jpg'
 ];
