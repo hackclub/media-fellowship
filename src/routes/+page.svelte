de5e2fc01951f2b8376cd4bb61b0b7e10feb59c1
astra.celestine <109295196+astra-the-boop@users.noreply.github.com>
2026-04-10 12:33:37 +0700

    Past events finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -385,28 +385,28 @@
 	<h1 class="stitle">In the past year, we have organized [PLACEHOLDER]</h1>
 	<div class="programs-grid">
 		<a class="program-card" href="https://flagship.hackclub.com" target="_blank" rel="noopener">
-			<img class="program-card-img" src="/images/campfire-flagship.png" alt="Campfire Flagship" />
-			<img class="program-card-logo" src="/images/campfire-flagship-logo.png" alt="Campfire Flagship logo" />
+			<img class="program-card-img" src="https://cdn.hackclub.com/019d75da-13a0-755c-9260-3a7a839e6c2b/image.png" alt="Campfire Flagship" />
+			<img class="program-card-logo" src="https://cdn.hackclub.com/019d75d9-1b8c-780a-a990-1c6b65473906/image.png" alt="Campfire Flagship logo" />
 			<p class="program-card-desc">A 3 day flagship game jam in Los Angeles with YouTubers like Michael Reeves, William Osman, and many more!</p>
 		</a>
 
 		<a class="program-card" href="https://midnight.hackclub.com" target="_blank" rel="noopener">
-			<img class="program-card-img" src="/images/midnight.jpg" alt="Midnight" />
+			<img class="program-card-img" src="https://manifesto.hackclub.com/images/midnight.jpg" alt="Midnight" />
 			<div class="midnight-logo">
-				<img class="front" src="/images/midnight-logo.svg" alt="Midnight logo" />
+				<img class="front" src="https://manifesto.hackclub.com/images/midnight-logo.svg" alt="Midnight logo" />
 			</div>
 			<p class="program-card-desc">A murder-mystery hackathon held in Austria, Vienna with 60+ teenagers from all across the world!</p>
 		</a>
 
 		<a class="program-card" href="https://blueprint.hackclub.com" target="_blank" rel="noopener">
-			<img class="program-card-img" src="/images/blueprint.png" alt="Blueprint" />
-			<img class="program-card-logo" src="/images/blueprint-logo.png" alt="Blueprint logo" style="width:300px" />
+			<img class="program-card-img" src="https://manifesto.hackclub.com/images/blueprint.png" alt="Blueprint" />
+			<img class="program-card-logo" src="https://cdn.hackclub.com/019d75da-1a49-739f-8e97-b3f6f15961ce/image.png" alt="Blueprint logo" style="width:300px" />
 			<p class="program-card-desc">From October 2025 to March 2026, Blueprint gave $140,000 to fund 1,500 hardware and electronics projects built by 1000 teenagers from 77 countries</p>
 		</a>
 
 		<a class="program-card" href="https://siege.hackclub.com" target="_blank" rel="noopener">
-			<img class="program-card-img" src="/images/siege.png" alt="Siege" />
-			<img class="program-card-logo" src="/images/siege-logo.png" alt="Siege logo" style="width:161px" />
+			<img class="program-card-img" src="https://manifesto.hackclub.com/images/siege.png" alt="Siege" />
+			<img class="program-card-logo" src="https://cdn.hackclub.com/019d75da-1ce9-7d50-9a52-53aadbfdfce7/image.png" alt="Siege logo" style="width:161px" />
 			<p class="program-card-desc">A 14 week event where 250 teens created a project each week to work towards a laptop with over 100 framework laptops given out.</p>
 		</a>
 	</div>
